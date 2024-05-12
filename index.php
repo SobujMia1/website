3a8d9ba65e587d5286e8cc2e33dca797ac89af12
SobujMia1 <sobujmia.bsfmstu@gmail.com>
2024-05-12 23:47:40 +0600

    all file added

---
+++ index.php
@@ -0,0 +1,52 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>| Online Quiz System |</title>
+    <link rel="stylesheet" type="text/css" href="css/index.css" />
+    <link rel="shortcut icon" type="image/png" href="image/logo.png" />
+    <style type="text/css">
+        body {
+            width: 100%;
+            background: url(image/book.png);
+            background-position: center center;
+            background-repeat: no-repeat;
+            background-attachment: fixed;
+            background-size: cover;
+        }
+
+        h2 {
+            color: #eeede6;
+            /* text-transform: uppercase; */
+            font-size: 30px;
+            font-weight: 500;
+            letter-spacing: 0.015em;
+        }
+        h3 {
+            color: #eeede6;
+            /* text-transform: uppercase; */
+            font-size: 20px;
+            font-weight: 500;
+            letter-spacing: 0.015em;
+        }
+    </style>
+</head>
+
+<body>
+    <center>
+        <div class="intro">
+            <h2>Bangamata Sheikh Fojilatunnesa Mujib Science & Technology University</h2>
+            <h2>Dept. Of Computer Science & Engineering</h2>
+            <h3>Course Title : Artificial Intelligence, Course Code: CSE 4131</h3>
+            <h1> online quiz system </h1>
+            <a href="login.php" class="btn"> login </a> &emsp;
+            <a href="register.php" class="btn"> register </a>
+            <a href="admin.php" class="btn"> admin </a>
+        </div>
+    </center>
+</body>
+
+</html>
